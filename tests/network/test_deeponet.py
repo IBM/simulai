7870a51d5510ad8b8588563676b7999638f19c44
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2023-01-02 12:25:47 -0300

    Unit tests for DeepONets combined to CNNs

--- tests/network/test_deeponet.py
+++ tests/network/test_deeponet.py
@@ -64,6 +64,55 @@ def model():
 
     return net
 
+def model_conv():
+
+    from simulai.regression import DenseNetwork, ConvolutionalNetwork
+    from simulai.models import DeepONet
+
+    n_inputs = 1
+    n_outputs = 2
+
+    n_latent = 50
+
+    # Configuration for the fully-connected trunk network
+    trunk_config = {
+        'layers_units': [50, 50, 50],  # Hidden layers
+        'activations': 'elu',
+        'input_size': 1,
+        'output_size': n_latent * n_outputs,
+        'name': 'trunk_net'
+    }
+
+    layers = [
+
+        {'in_channels': n_inputs, 'out_channels': 2, 'kernel_size': 3, 'stride': 1, 'padding': 1,
+         'after_conv': {'type': 'maxpool2d', 'kernel_size': 2, 'stride': 2}},
+
+        {'in_channels': 2, 'out_channels': 4, 'kernel_size': 3, 'stride': 1, 'padding': 1,
+         'after_conv': {'type': 'maxpool2d', 'kernel_size': 2, 'stride': 2}},
+
+        {'in_channels': 4, 'out_channels': 8, 'kernel_size': 3, 'stride': 1, 'padding': 1,
+         'after_conv': {'type': 'maxpool2d', 'kernel_size': 2, 'stride': 2}},
+
+        {'in_channels': 8, 'out_channels': n_latent * n_outputs, 'kernel_size': 3, 'stride': 1, 'padding': 1,
+         'after_conv': {'type': 'maxpool2d', 'kernel_size': 2, 'stride': 2}}
+    ]
+
+    # Instantiating and training the surrogate model
+    trunk_net = DenseNetwork(**trunk_config)
+    branch_net = ConvolutionalNetwork(layers=layers, activations='sigmoid', case='2d', name='net', flatten=True)
+
+    # It prints a summary of the network features
+    trunk_net.summary()
+    branch_net.summary(input_shape=[None, 1, 16, 16])
+
+    net = DeepONet(trunk_network=trunk_net,
+                   branch_network=branch_net,
+                   var_dim=n_outputs,
+                   model_id='deeponet')
+
+    return net
+
 class TestDeeponet(TestCase):
 
     def setUp(self) -> None:
@@ -78,7 +127,7 @@ class TestDeeponet(TestCase):
 
         output = net.forward(input_trunk=data_trunk, input_branch=data_branch)
         
-        print("Network has {net.n_parameters} parameters.")
+        print(f"Network has {net.n_parameters} parameters.")
         
         assert output.shape[1] == 2, "The network output is not like expected."
         
@@ -92,7 +141,7 @@ class TestDeeponet(TestCase):
         data_branch = torch.rand(1_000, 4)
         output_target = torch.rand(1_000, 2)
 
-        n_epochs = 1_000
+        n_epochs = 1_00
         maximum_values = (1 / np.linalg.norm(output_target, 2, axis=0)).tolist()
         params = {'lambda_1': 0.0, 'lambda_2': 1e-10, 'weights': maximum_values}
 
@@ -100,6 +149,51 @@ class TestDeeponet(TestCase):
 
         optimizer = Optimizer('adam', params=optimizer_config)
         net = model()
+
+        optimizer.fit(op=net, input_data=input_data, target_data=output_target,
+                      n_epochs=n_epochs, loss="wrmse", params=params, device='gpu')
+
+        output = net.forward(input_trunk=data_trunk, input_branch=data_branch)
+
+        assert output.shape[1] == 2, "The network output is not like expected."
+
+
+class TestDeeponet_with_Conv(TestCase):
+
+    def setUp(self) -> None:
+        pass
+
+    def test_deeponet_forward(self):
+
+        net = model_conv()
+
+        data_trunk = torch.rand(1_000, 1)
+        data_branch = torch.rand(1_000, 1, 16, 16)
+
+        output = net.forward(input_trunk=data_trunk, input_branch=data_branch)
+
+        print(f"Network has {net.n_parameters} parameters.")
+
+        assert output.shape[1] == 2, "The network output is not like expected."
+
+    def test_deeponet_train(self):
+
+        from simulai.optimization import Optimizer
+
+        optimizer_config = {'lr': 1e-3}
+
+        data_trunk = torch.rand(1_000, 1)
+        data_branch = torch.rand(1_000, 1, 16, 16)
+        output_target = torch.rand(1_000, 2)
+
+        n_epochs = 1_00
+        maximum_values = (1 / np.linalg.norm(output_target, 2, axis=0)).tolist()
+        params = {'lambda_1': 0.0, 'lambda_2': 1e-10, 'weights': maximum_values}
+
+        input_data = {'input_branch': data_branch, 'input_trunk': data_trunk}
+
+        optimizer = Optimizer('adam', params=optimizer_config)
+        net = model_conv()
 
         optimizer.fit(op=net, input_data=input_data, target_data=output_target,
                       n_epochs=n_epochs, loss="wrmse", params=params, device='gpu')
