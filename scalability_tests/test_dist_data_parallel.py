b4fd88342f6750fd694a717fbc6443b2ba45e8aa
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2023-01-04 16:33:14 -0300

    Multiprocessing

--- scalability_tests/test_dist_data_parallel.py
+++ scalability_tests/test_dist_data_parallel.py
@@ -3,6 +3,7 @@ import time
 import numpy as np
 import torch
 from torch.nn.parallel import DistributedDataParallel as DDP
+import torch.multiprocessing as mp
 
 from simulai.optimization import Optimizer
 
@@ -31,7 +32,7 @@ def model():
     # Configuration for the fully-connected trunk network
     trunk_config = {
         'layers_units': 7*[200],  # Hidden layers
-        'activations': 'elu',
+        'activations': 'tanh',
         'input_size': 1,
         'output_size': n_latent * n_outputs,
         'name': 'trunk_net'
@@ -40,18 +41,18 @@ def model():
     # CNN layers
     layers = [
 
-        {'in_channels': n_inputs, 'out_channels': 2, 'kernel_size': 3, 'stride': 1, 'padding': 1,
-         'after_conv': {'type': 'maxpool2d', 'kernel_size': 2, 'stride': 2}},
+                {'in_channels': n_inputs, 'out_channels': 2, 'kernel_size': 3, 'stride': 1, 'padding': 1,
+                 'after_conv': {'type': 'maxpool2d', 'kernel_size': 2, 'stride': 2}},
 
-        {'in_channels': 2, 'out_channels': 4, 'kernel_size': 3, 'stride': 1, 'padding': 1,
-         'after_conv': {'type': 'maxpool2d', 'kernel_size': 2, 'stride': 2}},
+                {'in_channels': 2, 'out_channels': 4, 'kernel_size': 3, 'stride': 1, 'padding': 1,
+                 'after_conv': {'type': 'maxpool2d', 'kernel_size': 2, 'stride': 2}},
 
-        {'in_channels': 4, 'out_channels': 8, 'kernel_size': 3, 'stride': 1, 'padding': 1,
-         'after_conv': {'type': 'maxpool2d', 'kernel_size': 2, 'stride': 2}},
+                {'in_channels': 4, 'out_channels': 8, 'kernel_size': 3, 'stride': 1, 'padding': 1,
+                 'after_conv': {'type': 'maxpool2d', 'kernel_size': 2, 'stride': 2}},
 
-        {'in_channels': 8, 'out_channels': n_latent * n_outputs, 'kernel_size': 3, 'stride': 1, 'padding': 1,
-         'after_conv': {'type': 'maxpool2d', 'kernel_size': 2, 'stride': 2}}
-    ]
+                {'in_channels': 8, 'out_channels': n_latent * n_outputs, 'kernel_size': 3, 'stride': 1, 'padding': 1,
+                 'after_conv': {'type': 'maxpool2d', 'kernel_size': 2, 'stride': 2}}
+             ]
 
     # Instantiating and training the surrogate model
     trunk_net = DenseNetwork(**trunk_config)
@@ -70,50 +71,50 @@ def model():
 
     return net
 
-n_ranks_list = [2, 4, 8]
 backend = 'nccl'
 
-def execute_demo():
-    for n in n_ranks_list:
+def execute_demo(n):
 
-        os.environ['MASTER_ADDR'] = 'localhost'
-        os.environ['MASTER_PORT'] = '12355'
+    os.environ['MASTER_ADDR'] = 'localhost'
+    os.environ['MASTER_PORT'] = '12355'
 
-        dist = torch.distributed.init_process_group(backend=backend, rank=0, world_size=n)
-        
-        rank = torch.distributed.get_rank()
-        print(f"Executing DDP job in {rank}.")
+    dist = torch.distributed.init_process_group(backend=backend, rank=0, world_size=n)
 
-        device_id = rank % torch.cuda.device_count()
+    rank = torch.distributed.get_rank()
+    print(f"Executing DDP job in {rank}.")
 
-        net = model().to(device_id)
+    device_id = rank % torch.cuda.device_count()
 
-        ddp_net = DDP(net, device_ids=[device_id])
+    net = model().to(device_id)
 
-        n_epochs = 100
+    ddp_net = DDP(net, device_ids=[device_id])
 
-        n_samples = 100_000
-        batch_size=1_000
-        lr = 1e-3  # Initial learning rate for the ADAM algorithm
-        optimizer_config = {'lr': lr}
+    n_epochs = 100
 
-        # Instantiating optimizer
-        params = {'lambda_1': 0., 'lambda_2': 0.}
-        optimizer_config = {'lr': lr}
-        optimizer = Optimizer('adam', params=optimizer_config)
+    n_samples = 100_000
+    batch_size=1_000
+    lr = 1e-3  # Initial learning rate for the ADAM algorithm
 
-        # Generating datasets
-        input_data, output_data = generate_data(n_samples=n_samples, image_size=(16, 16), n_inputs=1, n_outputs=16)
+    # Instantiating optimizer
+    params = {'lambda_1': 0., 'lambda_2': 1e-10}
+    optimizer_config = {'lr': lr}
+    optimizer = Optimizer('adam', params=optimizer_config)
 
+    # Generating datasets
+    input_data, output_data = generate_data(n_samples=n_samples, image_size=(16, 16), n_inputs=1, n_outputs=16)
 
-        ### Training
-        current_time = time.time()
-        optimizer.fit(op=ddp_net, input_data=input_data, target_data=output_data,
-                      n_epochs=n_epochs, loss="rmse", params=params, batch_size=batch_size)
-        elapsed_time = time.time() - current_time
+    ### Training
+    current_time = time.time()
+    optimizer.fit(op=ddp_net, input_data=input_data, target_data=output_data,
+                  n_epochs=n_epochs, loss="rmse", params=params, batch_size=batch_size)
+    elapsed_time = time.time() - current_time
 
-        print(f"Elapsed time for {n} ranks: {elapsed_time} s.")
+    print(f"Elapsed time for {n} ranks: {elapsed_time} s.")
 
 if __name__ == "__main__":
 
-    execute_demo()
+    n_ranks_list = [2, 4, 8]
+    
+    for n in n_ranks_list:
+
+        mp.spawn(execute_demo, args=(n,), nprocs=n, join=True)
