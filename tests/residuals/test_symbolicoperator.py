b81bff9d5d2a6eba09e0bc06308e66572a937e3c
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2023-01-12 13:40:01 -0300

    Directory of tests to evaluate symbolic residuals
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

---
+++ tests/residuals/test_symbolicoperator.py
@@ -0,0 +1,123 @@
+# (C) Copyright IBM Corp. 2019, 2020, 2021, 2022.
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#           http://www.apache.org/licenses/LICENSE-2.0
+
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+
+from unittest import TestCase
+import numpy as np
+import torch
+
+from simulai.residuals import SymbolicOperator
+
+def model():
+
+    from simulai.regression import DenseNetwork
+
+    input_labels = ['x', 't']
+    output_labels = ['u']
+
+    n_inputs = len(input_labels)
+    n_outputs = len(output_labels)
+
+    # Configuration for the fully-connected network
+    config = {
+        'layers_units': [128, 128, 128, 128],
+        'activations': 'sin',
+        'input_size': n_inputs,
+        'output_size': n_outputs,
+        'name': 'allen_cahn_net'
+    }
+
+    # Instantiating and training the surrogate model
+    net = DenseNetwork(**config)
+
+    return net
+
+class TestSymbolicOperator(TestCase):
+
+    def setUp(self) -> None:
+
+        pass
+
+    def test_symbolic_operator_1d_pde(self):
+
+
+        # Allen-cahn equation
+        f_0 = 'D(u, t) - mu*D(D(u, x), x) + alpha*(u**3) + beta*u'
+        # Invented 1
+        f_1 = 'D(D(u, t),t) - mu*D(D(u, x), x) + alpha*(u**3) + beta*u'
+        # Invented 2
+        f_2 = 'D(D(u, t), x) - D(mu*D(D(u, x), x), t) + alpha*(u**3) + beta*u'
+
+        g_u = 'u'
+        g_ux = 'D(u, x)'
+
+        input_labels = ['x', 't']
+        output_labels = ['u']
+
+        net = model()
+
+        # Generating the training grid
+        L = 1
+        x_0 = -1
+        T = 1
+        X_DIM = 100
+        T_DIM = 200
+        x_interval = [x_0, L]
+        t_interval = [0, T]
+
+        intervals = [x_interval, t_interval]
+
+        # Regular grid
+        x_0, x_L = x_interval
+        t_0, t_L = t_interval
+        dx = (x_L - x_0) / X_DIM
+        dt = (t_L - t_0) / T_DIM
+
+        grid = np.mgrid[t_0 + dt:t_L + dt:dt, x_0:x_L:dx]
+
+        data = np.hstack([grid[1].flatten()[:, None],
+                          grid[0].flatten()[:, None]])
+
+        # Boundary grids
+        data_boundary_x0 = np.hstack([x_interval[0] * np.ones((T_DIM, 1)),
+                                      np.linspace(*t_interval, T_DIM)[:, None]])
+
+        data_boundary_xL = np.hstack([x_interval[-1] * np.ones((T_DIM, 1)),
+                                      np.linspace(*t_interval, T_DIM)[:, None]])
+
+        for f in [f_0, f_1, f_2]:
+
+            residual = SymbolicOperator(expressions=[f], input_vars=input_labels,
+                                        auxiliary_expressions={'periodic_u': g_u, 'periodic_du': g_ux},
+                                        constants={'mu':1e-4, 'alpha':5, 'beta':-5},
+                                        output_vars=output_labels, function=net,
+                                        engine='torch')
+
+            # Verifying if all the PDE expressions are callable
+            assert all([callable(i) for i in residual.f_expressions])
+
+            # Verifying if all the boundary expressions are callable
+            try:
+                assert all([callable(i) for i in residual.g_expressions.values()])
+            except:
+                assert all([callable(i) for i in residual.g_expressions])
+
+            # Testing to evaluate a list of PDE expressions
+            assert all([isinstance(item, torch.Tensor) for item in residual(data)])
+
+            # Testing to evaluate auxiliary (boundary) expressions
+            assert isinstance(residual.eval_expression('periodic_u', [data_boundary_xL, data_boundary_x0]),
+                              torch.Tensor)
+            assert isinstance(residual.eval_expression('periodic_u', [data_boundary_xL, data_boundary_x0]),
+                              torch.Tensor)
