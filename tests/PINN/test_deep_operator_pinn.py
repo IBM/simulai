bc053316173d2ef23b4642bceed9f26be114f711
João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>
2023-01-25 12:29:52 -0300

    Basic tests for PINNs
    Signed-off-by: João Lucas de Sousa Almeida <joao.l.sa.9.3@gmail.com>

---
+++ tests/PINN/test_deep_operator_pinn.py
@@ -0,0 +1,141 @@
+# (C) Copyright IBM Corp. 2019, 2020, 2021, 2022.
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#           http://www.apache.org/licenses/LICENSE-2.0
+
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+import numpy as np
+
+from simulai.optimization import Optimizer
+from simulai.residuals import SymbolicOperator
+
+class TestDeepOperatorROBERPINN:
+
+    def __init__(self):
+        pass
+
+    def test_rober_deeponet_pinn(self):
+
+        Q = 1_00
+        N = int(5)
+
+        k1 = 0.04
+        k2 = 3e7
+        k3 = 1e4
+
+        t_intv = [0, 1]
+        s_intv = np.stack([[0, 0, 0], [1, 1e-4, 1]], axis=0)
+
+        # The expression we aim at minimizing
+        f_s1 = "D(s1, t) + k1*s1 - k3*s2*s3"
+        f_s2 = "D(s2, t) - k1*s1 + k2*(s2**2) + k3*s2*s3"
+        f_s3 = "D(s3, t) - k2*(s2**2)"
+
+        U_t = np.random.uniform(low=t_intv[0], high=t_intv[1], size=Q)
+        U_s = np.random.uniform(low=s_intv[0], high=s_intv[1], size=(N, 3))
+
+        branch_input_train = np.tile(U_s[:, None, :], (1, Q, 1)).reshape(N*Q, -1)
+        trunk_input_train = np.tile(U_t[:, None], (N, 1))
+
+        initial_states = U_s
+
+        input_labels = ['t']
+        output_labels = ['s1', 's2', 's3']
+
+        lambda_1 = 0.0  # Penalty for the L¹ regularization (Lasso)
+        lambda_2 = 0.0  # Penalty factor for the L² regularization
+        n_epochs = 40 # Maximum number of iterations for ADAM
+        lr = 1e-3  # Initial learning rate for the ADAM algorithm
+
+        def model():
+
+            import numpy as np
+
+            from simulai.regression import ConvexDenseNetwork, SLFNN
+            from simulai.models import ImprovedDeepONet
+
+            n_latent = 100
+            n_inputs_b = 3
+            n_inputs_t = 1
+            n_outputs = 3
+
+            # Configuration for the fully-connected trunk network
+            trunk_config = {
+                'layers_units': 3 * [20],  # Hidden layers
+                'activations': 'tanh',
+                'input_size': n_inputs_t,
+                'output_size': n_latent * n_outputs,
+                'name': 'trunk_net'
+            }
+
+            # Configuration for the fully-connected branch network
+            branch_config = {
+                'layers_units': 3 * [20],  # Hidden layers
+                'activations': 'tanh',
+                'input_size': n_inputs_b,
+                'output_size': n_latent * n_outputs,
+                'name': 'branch_net',
+            }
+
+            # Instantiating and training the surrogate model
+            trunk_net = ConvexDenseNetwork(**trunk_config)
+            branch_net = ConvexDenseNetwork(**branch_config)
+
+            encoder_trunk = SLFNN(input_size=n_inputs_t, output_size=20, activation='tanh')
+            encoder_branch = SLFNN(input_size=n_inputs_b, output_size=20, activation='tanh')
+
+            # It prints a summary of the network features
+            trunk_net.summary()
+            branch_net.summary()
+
+            rober_net = ImprovedDeepONet(trunk_network=trunk_net,
+                                         branch_network=branch_net,
+                                         encoder_trunk=encoder_trunk,
+                                         encoder_branch=encoder_branch,
+                                         var_dim=n_outputs,
+                                         rescale_factors=np.array([1, 1e-4, 1]),
+                                         devices='gpu',
+                                         model_id='rober_net')
+
+            return rober_net
+
+        rober_net = model()
+
+        residual = SymbolicOperator(expressions=[f_s1, f_s2, f_s3], input_vars=input_labels,
+                                    output_vars=output_labels, function=rober_net,
+                                    inputs_key='input_trunk',
+                                    constants={"k1": k1, "k2": k2,
+                                               "k3": k3},
+                                    device='gpu',
+                                    engine='torch')
+
+        # Maximum derivative magnitudes to be used as loss weights
+        penalties = [1, 1e6, 1]
+
+        optimizer_config = {'lr': lr}
+
+        input_data = {'input_branch': branch_input_train, 'input_trunk': trunk_input_train}
+
+        optimizer = Optimizer('adam', params=optimizer_config, lr_decay_scheduler_params={'name': 'ExponentialLR',
+                                                                                                  'gamma': 0.9,
+                                                                                                  'decay_frequency': 5_000},
+                              summary_writer=True)
+
+        params = {'lambda_1': lambda_1,
+                  'lambda_2': lambda_2,
+                  'residual': residual,
+                  'initial_input': {'input_trunk': np.zeros((N, 1)), 'input_branch': initial_states},
+                  'initial_state': initial_states,
+                  'weights_residual': [1, 1, 1],
+                  'weights': penalties}
+
+        optimizer.fit(op=rober_net, input_data=input_data,
+                      n_epochs=n_epochs, loss="opirmse", params=params, device='gpu')
---
+++ tests/PINN/test_vanilla_pinn.py
@@ -0,0 +1,135 @@
+# (C) Copyright IBM Corp. 2019, 2020, 2021, 2022.
+
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+
+#           http://www.apache.org/licenses/LICENSE-2.0
+
+#     Unless required by applicable law or agreed to in writing, software
+#     distributed under the License is distributed on an "AS IS" BASIS,
+#     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#     See the License for the specific language governing permissions and
+#     limitations under the License.
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+from simulai.optimization import Optimizer
+from simulai.residuals import SymbolicOperator
+
+class TestAllencahnPINN:
+
+    def __init__(self):
+        pass
+
+    def test_allen_cahn(self):
+
+        # Our PDE
+        # Allen-cahn equation
+
+        f = 'D(u, t) - mu*D(D(u, x), x) + alpha*(u**3) + beta*u'
+
+        g_u = 'u'
+        g_ux = 'D(u, x)'
+
+        input_labels = ['x', 't']
+        output_labels = ['u']
+
+        # Some fixed values
+        X_DIM = 25
+        T_DIM = 10
+
+        L = 1
+        x_0 = -1
+        T = 1
+
+        # Generating the training grid
+
+        x_interval = [x_0, L]
+        t_interval = [0, T]
+
+        intervals = [x_interval, t_interval]
+
+        # Regular grid
+        x_0, x_L = x_interval
+        t_0, t_L = t_interval
+        dx = (x_L - x_0) / X_DIM
+        dt = (t_L - t_0) / T_DIM
+
+        grid = np.mgrid[t_0+dt:t_L+dt:dt, x_0:x_L:dx]
+
+        data = np.hstack([grid[1].flatten()[:, None],
+                          grid[0].flatten()[:, None]])
+
+        data_init = np.linspace(*x_interval, X_DIM)
+        u_init = (data_init**2)*np.cos(np.pi * data_init)[:, None]
+
+        # Boundary grids
+        data_boundary_x0 = np.hstack([x_interval[0] * np.ones((T_DIM, 1)),
+                                      np.linspace(*t_interval, T_DIM)[:, None]])
+
+        data_boundary_xL = np.hstack([x_interval[-1] * np.ones((T_DIM, 1)),
+                                      np.linspace(*t_interval, T_DIM)[:, None]])
+
+        data_boundary_t0 = np.hstack([np.linspace(*x_interval, X_DIM)[:, None],
+                                      t_interval[0] * np.ones((X_DIM, 1))])
+
+        n_epochs = 50  # Maximum number of iterations for ADAM
+        lr = 1e-3  # Initial learning rate for the ADAM algorithm
+
+        def model():
+
+            from simulai.regression import DenseNetwork
+
+            input_labels = ['x', 't']
+            output_labels = ['u']
+
+            n_inputs = len(input_labels)
+            n_outputs = len(output_labels)
+
+            # Configuration for the fully-connected network
+            config = {
+                        'layers_units': [128, 128, 128, 128],
+                        'activations': 'tanh',
+                        'input_size': n_inputs,
+                        'output_size': n_outputs,
+                        'name': 'allen_cahn_net'
+                     }
+
+            # Instantiating and training the surrogate model
+            net = DenseNetwork(**config)
+
+            return net
+
+        optimizer_config = {'lr': lr}
+
+        net = model()
+
+        residual = SymbolicOperator(expressions=[f], input_vars=input_labels,
+                                    auxiliary_expressions={'periodic_u': g_u, 'periodic_du': g_ux},
+                                    constants={'mu':1e-4, 'alpha':5, 'beta':-5},
+                                    output_vars=output_labels, function=net,
+                                    engine='torch',
+                                    device='gpu')
+
+        # It prints a summary of the network features
+        net.summary()
+
+        optimizer = Optimizer('adam', params=optimizer_config, lr_decay_scheduler_params={'name': 'ExponentialLR',
+                                                                                          'gamma': 0.9,
+                                                                                          'decay_frequency': 5_000},
+                              shuffle=False,
+                              summary_writer=True)
+
+        params = {'residual': residual,
+                  'initial_input': data_boundary_t0,
+                  'initial_state': u_init,
+                  'boundary_input': {'periodic_u': [data_boundary_xL, data_boundary_x0],
+                                     'periodic_du': [data_boundary_xL, data_boundary_x0]},
+                  'boundary_penalties': [1, 1],
+                  'initial_penalty': 100}
+
+        optimizer.fit(op=net, input_data=data,
+                      n_epochs=n_epochs, loss="pirmse", params=params, device='gpu')
+
