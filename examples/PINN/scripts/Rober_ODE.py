4a445ff877b3e273996f38d8bd8009f36ac402f5
Nicolas Spogis <nicolas.spogis@gmail.com>
2023-05-17 09:13:20 -0300

    Rober Cases
    Remove - Rober_edo.py (Portuguese name)
    Add - Rober_ODE.py (English Name)
    Add - ROber Multifidelity PINN Model

--- examples/PINN/scripts/Rober_ODE.py
+++ examples/PINN/scripts/Rober_ODE.py
@@ -23,9 +23,9 @@ LinkTree: https://linktr.ee/spogis
 
 """    Import Python Libraries    """
 
-import matplotlib.pyplot as plt
-import numpy as np
 import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy.integrate import solve_ivp
 
 """    Global Variables    """
@@ -34,22 +34,20 @@ k1 = 0.04
 k2 = 3e7
 k3 = 1e4
 
-Simulated_Time = 500
+Simulated_Time = 500           
 
 """    Differential Equations    """
 
-
 def model(t, y):
-    s1 = y[0]
-    s2 = y[1]
-    s3 = y[2]
+  s1 =  y[0]
+  s2 =  y[1]
+  s3 =  y[2]
+  
+  ds1dt = -k1 * s1 + k3 * s2 * s3
+  ds2dt = k1 * s1 - k2 * (s2**2) - k3 * s2 * s3
+  ds3dt = k2 * (s2**2)
 
-    ds1dt = -k1 * s1 + k3 * s2 * s3
-    ds2dt = k1 * s1 - k2 * (s2**2) - k3 * s2 * s3
-    ds3dt = k2 * (s2**2)
-
-    return [ds1dt, ds2dt, ds3dt]
-
+  return [ds1dt, ds2dt, ds3dt]
 
 """     Initial Conditions    """
 s1_0 = 1.0
@@ -58,15 +56,13 @@ s3_0 = 0.0
 Initial_Conditions = [s1_0, s2_0, s3_0]
 
 # In order to use on Plots
-Variable_Names = ["s1", "s2", "s3"]
+Variable_Names= ['s1', 's2', 's3']
 
 """     Solve ODE       """
 
-t_eval = np.linspace(0, Simulated_Time, num=50000, endpoint=True)
+t_eval = np.linspace(0, Simulated_Time, num=1000, endpoint=True)
 
-sol = solve_ivp(
-    model, [0, Simulated_Time], Initial_Conditions, method="LSODA", t_eval=t_eval
-)
+sol = solve_ivp(model, [0, Simulated_Time], Initial_Conditions, method = 'LSODA', t_eval=t_eval)
 
 """     Generating a More Usable Dataset    """
 
@@ -75,26 +71,12 @@ y = np.transpose(sol.y)
 
 y = y * np.array([1, 1e4, 1])
 
-df_time = pd.DataFrame({"Time": t})
+df_time = pd.DataFrame({'Time':t})
 
-df_num_sol = pd.DataFrame(y, columns=Variable_Names)
+df_num_sol = pd.DataFrame(y , columns = Variable_Names)
 
-Results = pd.concat([df_time, df_num_sol], axis=1)
+Results = pd.concat([df_time,df_num_sol], axis=1)
 
 """     Export Results for PINN Performance Evaluation    """
 
-Results.to_csv("Rober_EDO.csv", index=False)
-
-"""     Plot Results    """
-
-df_PINN = pd.read_csv("Rober_PINN.csv")
-
-plt.figure(2)
-plt.plot(Results["Time"], Results["s1"], label="s1 EDO")
-plt.plot(Results["Time"], Results["s2"], label="s2 EDO")
-plt.plot(Results["Time"], Results["s3"], label="s3 EDO")
-plt.plot(df_PINN["Time"], df_PINN["s1"], label="s1 PINN")
-plt.plot(df_PINN["Time"], df_PINN["s2"], label="s2 PINN")
-plt.plot(df_PINN["Time"], df_PINN["s3"], label="s3 PINN")
-plt.legend()
-plt.xlabel("Time")
+Results.to_csv("Rober_ODE.csv", index=False)
