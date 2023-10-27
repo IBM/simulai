4199a077bef958c72499431ac563b59e5fe3e8b4
João Lucas de Sousa Almeida <Joao.Lucas.Sousa.Almeida@ibm.com>
2023-10-27 14:26:28 -0300

    Simple API to torchview
    Signed-off-by: João Lucas de Sousa Almeida <Joao.Lucas.Sousa.Almeida@ibm.com>

---
+++ simulai/utilities/_vizualization.py
@@ -0,0 +1,28 @@
+from typing import Dict, Union
+import torch
+
+def view_api(module:torch.nn.Module=None,
+             input_data: Union[torch.nn.Module, np.ndarray]=None,
+             config:Dict=None, save_dir:str=None):
+
+    try:
+        from torchview import draw_graph
+    except Exception:
+        raise Exception("For using the visualization API,\
+                        it is necessary to install the torchview module.")
+    
+    # Wrapper for torchview
+    class Module_wrap(torch.nn.Module):
+        def __init__(self, module=None):
+            super(Module_wrap, self).__init__()
+            self.module = module
+        def forward(self,*args, **kwargs):
+            return self.module(*args)
+        def train(self, *args, **kwargs):
+            pass
+        def eval(self, *args, **kwargs):
+            pass 
+
+    module_wrap = Module_wrap(module=module)
+
+    draw_graph(module_wrap,input_data=input_data, graph_dir=save_dir, **config)
